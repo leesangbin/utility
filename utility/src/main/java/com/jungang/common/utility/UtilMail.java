d59dbc53ceed459c05e9ff50c69b8d1130d80087
leesangbin <sangbinlee9@gmail.com>
2015-12-10 13:08:18 +0900

    삭제 및 수정

--- utility/src/main/java/com/jungang/common/utility/UtilMail.java
+++ utility/src/main/java/com/jungang/common/utility/UtilMail.java
@@ -1,8 +1,22 @@
 package com.jungang.common.utility;
 
+/**
+ * <pre>
+ * @author Administrator
+ *1. A simple text email (받는사람, 참조, 숨은참조, MAIL제목, MAIL내용)
+ *2. Sending emails with attachments
+ *3. Sending HTML formatted email
+ *4. Sending HTML formatted email with embedded images
+ *</pre>
+ *
+ *
+ */
 public interface UtilMail {
 	public void sendEmail(String[] toAddrs, String[] toCc, String[] toBcc, String mailSubject, String mailMsg);
+
 	public void sendEmail(String imageFilePath, String[] toAddrs, String[] toCc, String[] toBcc, String mailSubject, String mailMsg);
+
 	public void sendEmail(String htmlFilePath, String imageFilePath, String[] toAddrs, String[] toCc, String[] toBcc, String mailSubject, String mailMsg);
+
 	public void sendEmail(String urlString, String htmlFilePath, String imageFilePath, String[] toAddrs, String[] toCc, String[] toBcc, String mailSubject, String mailMsg);
 }
